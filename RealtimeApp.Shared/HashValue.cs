81290e74160780c2cf090d7c0211a77d4d9868f9
aradhyap <manuaradhya07@gmail.com>
2023-01-20 18:44:53 +0530

    Moved HashValue to separate proj

---
+++ RealtimeApp.Shared/HashValue.cs
@@ -0,0 +1,63 @@
+﻿using System.Security.Cryptography;
+
+namespace RealtimeApp.Shared;
+
+public readonly struct HashValue
+{
+    private static readonly SHA256 Sha256;
+    public readonly byte[] Hash;
+
+    #region CTOR
+    
+    public HashValue(byte[] hash)
+    {
+        Hash = hash;
+    }
+    static HashValue()
+    {
+        Sha256 = SHA256.Create();
+    }
+    
+    #endregion
+    
+    public static HashValue Generate(byte[] value)
+    {
+        Span<byte> hash = Sha256.ComputeHash(value);
+        return new HashValue(hash.Slice(0, 16).ToArray());
+    }
+    
+    public bool Compare(byte[] other)
+    {
+        if (other.Length != Hash.Length)
+        {
+            return false;
+        }
+        for (var i = 0; i < Hash.Length; i++)
+        {
+            if (Hash[i] != other[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public bool Compare(HashValue otherValue)
+    {
+        var other = otherValue.Hash;
+
+        if (other.Length != Hash.Length)
+        {
+            return false;
+        }
+        for (int i = 0; i < Hash.Length; i++)
+        {
+            if (Hash[i] != other[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static implicit operator byte[](HashValue v) => v.Hash;
+}
--- RealtimeApp.Tests/Program.cs
+++ RealtimeApp.Tests/Program.cs
@@ -1,13 +1,15 @@
 ﻿//PacketTests.Run();
 
+using RealtimeApp;
 using RealtimeApp.Shared;
+using RealtimeApp.Tests;
 
 using var writerPacket = new WriterPacket();
 writerPacket.WriteBytes(RealtimeApp.Tests.Constants.GetHash().Hash);
 writerPacket.WriteString("username");
 writerPacket.WriteString("password");
 
-var hash1 = RealtimeApp.Tests.Constants.GetHash(writerPacket);
-var hash2 = RealtimeApp.Tests.Constants.GetHash(writerPacket);
+var hash1 = HashValue.Generate(writerPacket);
+var hash2 = HashValue.Generate(writerPacket);
 
 Console.WriteLine(hash1.Compare(hash2));
