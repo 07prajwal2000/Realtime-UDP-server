534478bf19d3f6978fcac4e77119033c05a6a8e6
07prajwal2000 <manuaradhya07@gmail.com>
2023-01-22 23:45:32 +0530

    Some raygui tests

--- RealtimeApp.Tests/Program.cs
+++ RealtimeApp.Tests/Program.cs
@@ -1,50 +1,49 @@
-﻿//PacketTests.Run();
-
-//using RealtimeApp.Shared;
-
-//using var writerPacket = new WriterPacket();
-//writerPacket.WriteBytes(RealtimeApp.Tests.Constants.GetHash().Hash);
-//writerPacket.WriteString("username");
-//writerPacket.WriteString("password");
-
-//var hash1 = RealtimeApp.Tests.Constants.GetHash(writerPacket);
-//var hash2 = RealtimeApp.Tests.Constants.GetHash(writerPacket);
-
-//Console.WriteLine(hash1.Compare(hash2));
-
-//using Microsoft.Extensions.DependencyInjection;
-//using SuperSimpleTcp;
-
-//var services = new ServiceCollection();
-//services.AddSingleton(new SimpleTcpServer("127.0.0.1", 8888));
-
-//var builder = services.BuildServiceProvider();
-
-//var tcpServer = builder.GetRequiredService<SimpleTcpServer>();
-
-//tcpServer.Events.ClientConnected += OnClientConnected;
-//tcpServer.Events.ClientDisconnected += OnClientDisconnected;
-//tcpServer.Events.DataReceived += OnDataReceived;
-
-//void OnDataReceived(object? sender, DataReceivedEventArgs e)
-//{
-
-//}
-
-//void OnClientDisconnected(object? sender, ConnectionEventArgs e)
-//{
-
-//}
-
-//tcpServer.Events.ClientConnected += OnClientConnected;
-
-//void OnClientConnected(object? sender, ConnectionEventArgs e)
-//{
-
-//}
-
+﻿
 //tcpServer.Start();
 
+using Raylib_CsLo;
 using RealtimeApp.Tests.RaylibTest;
 
-await Runner.Run();
+//await Runner.Run();
+
+Raylib.InitWindow(900, 480, "TEST");
+Raylib.SetWindowState(ConfigFlags.FLAG_WINDOW_RESIZABLE);
+Raylib.SetTargetFPS(60);
+var show = false;
+
+// Main game loop
+while (!Raylib.WindowShouldClose()) // Detect window close button or ESC key
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Raylib.SKYBLUE);
+    Raylib.DrawFPS(10, 10);
+    Update();
+    Raylib.EndDrawing();
+}
+
+void Update()
+{
+    if (RayGui.GuiButton(new Rectangle(50, 25, 100, 30), "Click me"))
+    {
+        Console.WriteLine("Clicked");
+        show = true;
+        
+    }
+
+    if (show)
+    {
+        var clicked = RayGui.GuiMessageBox(new Rectangle(50, 50, 500, 400), "message box", "you clicked", "yes;no");
+        if (clicked == 1)
+        {
+            Console.WriteLine(clicked == 0 ? "yes" : "no");
+            show = false;
+        }
+        else if(clicked == 2 || clicked == 0)
+        {
+            show = false;
+        }
+    }
+
+}
+
+Raylib.CloseWindow();
